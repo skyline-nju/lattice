901ab01f30003587beec54fd15264e5e463b33fb
skyline-nju <duanyu.nju@hotmail.com>
2019-12-05 16:52:19 +0800

    delete PackedSquareLattice_2

--- PEP_v2/PEP.cpp
+++ PEP_v2/PEP.cpp
@@ -2,21 +2,17 @@
 #include "PEP.h"
 #include "latticeExporter.h"
 
-template <>
-const short SquarePar_2<short>::ori[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
-
 int main(int agrc, char* argv[]) {
-  int Lx = 1000;
-  int Ly = 4000;
-  double phi = 0.01;
+  int Lx = 500;
+  int Ly = 500;
+  double phi = 0.1;
   double alpha = 0.01;
-  int n_step = 10000;
-
-  std::vector<SquarePar_2<short>> par_arr;
-  //SimpleSquareLattice_2 lattice(Lx, Ly);
-  PackedSquareLattice_2 lattice(Lx, Ly, 4, 3);
-  Ranq1 myran(2);
-  create_rand(phi, par_arr, lattice, myran);
+  int n_step = 50000;
+  Ran myran(2);
+  
+  std::vector<Par_2> par_arr;
+  SquareLattice_2 lattice(Lx, Ly);
+  lattice.create_particles_randomly(par_arr, myran, phi);
   std::vector<size_t> seq;
   seq.reserve(par_arr.size());
   for (int i = 0; i < par_arr.size(); i++) {
@@ -32,8 +28,12 @@ int main(int agrc, char* argv[]) {
 
   xy.dump(0, par_arr);
   for (int i = 1; i <= n_step; i++) {
-    run_in_rand_seq(par_arr, lattice, myran, seq, alpha);
+    shuffle(seq, myran);
+    update_in_rand_seq(par_arr, lattice, myran, seq, alpha);
     log.record(i);
     xy.dump(i, par_arr);
   }
+
 }
+
+
--- PEP_v2/PEP.h
+++ PEP_v2/PEP.h
@@ -2,183 +2,108 @@
 #include "config.h"
 #include "rand.h"
 
-template <typename T>
-struct Pos_2 {
-  Pos_2() = default;
-  Pos_2(T a, T b) : x(a), y(b) {}
-
-  T x;
-  T y;
-};
-
-template <typename T>
-struct SquarePar_2 {
-  SquarePar_2() = default;
-  template <typename T2>
-  SquarePar_2(const Pos_2<T2>& pos, unsigned char s0) : x(pos.x), y(pos.y), s(s0) {}
-  Pos_2<T> jump() const { return Pos_2<T>(x + ori[s][0], y + ori[s][1]); }
-  void reset_pos(const Pos_2<T>& pos) { x = pos.x; y = pos.y; }
-
-  T x;
-  T y;
-  unsigned char s;
-
-  const static int size_s = 4;
-  const static short ori[4][2];
-};
-
-class SimpleSquareLattice_2 {
+struct Par_2 {
 public:
-  SimpleSquareLattice_2(int Lx, int Ly) : Lx_(Lx), Ly_(Ly) { n_par_ = new unsigned char[Lx * Ly](); }
-  ~SimpleSquareLattice_2() { delete[] n_par_; }
+  Par_2() = default;
+  Par_2(short a, short b, unsigned char s) : x(a), y(b), ori(s) {}
+  short x;
+  short y;
+  unsigned char ori;
+};
 
-  size_t get_vol() const { return Lx_ * Ly_; }
+class SquareLattice_2 {
+public:
+  SquareLattice_2(int Lx0, int Ly0)
+    : Lx_(Lx0), Ly_(Ly0), tot_lattices_(static_cast<size_t>(Lx_) * Ly_) { 
+    n_ = new unsigned char[tot_lattices_](); }
+
+  ~SquareLattice_2() { delete[] n_; }
+
+  template <typename TRan>
+  void create_particles_randomly(std::vector<Par_2>& p_arr, TRan& myran, double phi);
+
+  template <typename TRan>
+  void tumble(Par_2& p, TRan& myran, double alpha) const;  
+  template <typename TPar>
+  void move(TPar& p);
 
   template <typename T>
-  unsigned char get_np(T x, T y) const { return n_par_[x + y * Lx_]; }
+  void boundary_condi(T &x, T &y) const;
 
-  template <typename TPar, typename TPos>
-  void try_move(TPar& p, const TPos& pos_new);
-
-  template <typename TRan, typename TPos>
-  void add_one_randomly(TRan& myran, TPos& pos);
-
+private:
   int Lx_;
   int Ly_;
-
-protected:
-  unsigned char* n_par_;
+  const size_t tot_lattices_;
+  unsigned char* n_;
+  const int n_oris_ = 4;
+  const short ori_arr_[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 };
 
-class PackedSquareLattice_2 {
-public:
-  PackedSquareLattice_2(int Lx, int Ly, int nj, int base)
-    : Lx_(Lx), Ly_(Ly), ncols_(Lx_), nrows_(Ly_ / nj), nj_(nj), base_(base) {
-    n_par_ = new unsigned char[ncols_ * nrows_]();
-  }
-
-  ~PackedSquareLattice_2() { delete[] n_par_; }
-
-  size_t get_vol() const { return Lx_ * Ly_; }
-
-  template <typename TPar, typename TPos>
-  void try_move(TPar& p, const TPos& pos_new);
-
-  template <typename TRan, typename TPos>
-  void add_one_randomly(TRan& myran, TPos& pos);
-
-  int Lx_;
-  int Ly_;
-
-protected:
-  int ncols_;
-  int nrows_;
-  int nj_;
-  int base_;
-  unsigned char* n_par_;
-
-  const int base_arr_[4] = { 1, 3, 9, 27 };
-};
-
-
-template<typename TPar, typename TPos>
-void SimpleSquareLattice_2::try_move(TPar& p, const TPos& pos_new) {
-  size_t idx_new = pos_new.x + pos_new.y * Lx_;
-  if (n_par_[idx_new] < N_MAX) {
-    size_t idx_old = p.x + p.y * Lx_;
-    n_par_[idx_new]++;
-    n_par_[idx_old]--;
-    p.reset_pos(pos_new);
-  }
-}
-
-template<typename TRan, typename TPos>
-void SimpleSquareLattice_2::add_one_randomly(TRan& myran, TPos& pos) {
-  while (true) {
-    pos.x = myran.doub() * Lx_;
-    pos.y = myran.doub() * Ly_;
-    size_t idx = pos.x + pos.y * Lx_;
-    if (n_par_[idx] < N_MAX) {
-      n_par_[idx]++;
-      break;
-    }
-  }
-}
-
-template<typename TPar, typename TPos>
-void PackedSquareLattice_2::try_move(TPar& p, const TPos& pos_new) {
-  auto x_lat_new = pos_new.x;
-  auto y_lat_new = pos_new.y / nj_;
-  auto j_new = pos_new.y - y_lat_new * nj_;
-  size_t idx_new = x_lat_new + y_lat_new * ncols_;
-  unsigned char np_new = (n_par_[idx_new] / base_arr_[j_new]) % base_;
-  if (np_new < N_MAX) {
-    auto x_lat = p.x;
-    auto y_lat = p.y / nj_;
-    auto j = p.y - y_lat * nj_;
-    size_t idx = x_lat + y_lat * ncols_;
-    n_par_[idx_new] += base_arr_[j_new];
-    n_par_[idx] -= base_arr_[j];
-    p.reset_pos(pos_new);
-  }
-}
-
-template<typename TRan, typename TPos>
-void PackedSquareLattice_2::add_one_randomly(TRan& myran, TPos& pos) {
-  while (true) {
-    pos.x = myran.doub() * Lx_;
-    pos.y = myran.doub() * Ly_;
-    auto x_lat = pos.x;
-    auto y_lat = pos.y / nj_;
-    auto j = pos.y - y_lat * nj_;
-    size_t idx = x_lat + y_lat * ncols_;
-    unsigned char np = (n_par_[idx] / base_arr_[j]) % base_;
-    if (np < N_MAX) {
-      n_par_[idx] += base_arr_[j];
-      break;
-    }
-  }
-}
-
-template<typename TPos, typename TLat>
-void boundary_condi(TPos& p, const TLat& lat) {
-  if (p.x < 0) {
-    p.x = lat.Lx_ - 1;
-  } else if (p.x >= lat.Lx_) {
-    p.x = 0;
-  }
-
-  if (p.y < 0) {
-    p.y = lat.Ly_ - 1;
-  } else if (p.y >= lat.Ly_) {
-    p.y = 0;
-  }
-}
-
-template <typename TPar, typename TLattice, typename TRan>
-void create_rand(double phi, std::vector<TPar>& p_arr, TLattice& lat, TRan& myran) {
-  size_t n_par = phi * lat.get_vol();
+template <typename TRan>
+void SquareLattice_2::create_particles_randomly(std::vector<Par_2>& p_arr, TRan& myran, double phi) {
+  size_t n_par = phi * tot_lattices_;
   p_arr.reserve(n_par);
   for (int j = 0; j < n_par; j++) {
-    unsigned char s_new = static_cast<unsigned char>(myran.doub() * TPar::size_s);
-    Pos_2<short> pos_new;
-    lat.add_one_randomly(myran, pos_new);
-    p_arr.emplace_back(pos_new, s_new);
+    unsigned char ori = static_cast<unsigned char>(myran.doub() * n_oris_);
+    short x;
+    short y;
+    while (true) {
+      x = static_cast<short>(myran.doub() * Lx_);
+      y = static_cast<short>(myran.doub() * Ly_);
+      size_t idx = x + y * Lx_;
+      if (n_[idx] < N_MAX) {
+        n_[idx]++;
+        break;
+      }
+    }
+    p_arr.emplace_back(x, y, ori);
+  }
+}
+
+template <typename T>
+void SquareLattice_2::boundary_condi(T &x, T&y) const {
+  if (x < 0) {
+    x = Lx_ - 1;
+  } else if (x >= Lx_) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = Ly_ - 1;
+  } else if (y >= Ly_) {
+    y = 0;
+  }
+}
+
+template<typename TRan>
+void SquareLattice_2::tumble(Par_2& p, TRan& myran, double alpha) const {
+  if (myran.doub() < alpha) {
+    p.ori = static_cast<unsigned char>(myran.doub() * n_oris_);
+  }
+}
+
+template <typename TPar>
+void SquareLattice_2::move(TPar& p) {
+  short x = p.x + ori_arr_[p.ori][0];
+  short y = p.y + ori_arr_[p.ori][1];
+  boundary_condi(x, y);
+  size_t idx1 = x + y * static_cast<size_t>(Lx_);
+  if (n_[idx1] < N_MAX) {
+    size_t idx0 = p.x + p.y * static_cast<size_t>(Lx_);
+    n_[idx0]--;
+    n_[idx1]++;
+    p.x = x;
+    p.y = y;
   }
 }
 
 template <typename TPar, typename TLattice, typename TRan>
-void run_in_rand_seq(std::vector<TPar>& p_arr, TLattice& lat, TRan& myran,
+void update_in_rand_seq(std::vector<TPar>& p_arr, TLattice& lat, TRan& myran,
   std::vector<size_t>& idx_arr, double alpha) {
-  shuffle(idx_arr, myran);
   auto end = idx_arr.cend();
   for (auto it = idx_arr.cbegin(); it != end; ++it) {
     auto& p = p_arr[*it];
-    if (myran.doub() < alpha) {
-      p.s = static_cast<unsigned char>(myran.doub() * TPar::size_s);
-    }
-    auto pos_new = p.jump();
-    boundary_condi(pos_new, lat);
-    lat.try_move(p, pos_new);
+    lat.tumble(p, myran, alpha);
+    lat.move(p);
   }
 }
